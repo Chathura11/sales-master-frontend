f295b90ca49ad8df49fb29cf3d6eb5daecb7c27b
chathura <chathuraasela11@gmail.com>
2025-03-28 09:09:35 +0530

    modified

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   withCredentials: true,
-  baseURL: 'http://localhost:4040/api/',
+  baseURL: 'https://sales-master-backend.onrender.com/api/',
 });
 
 export default axiosInstance;
